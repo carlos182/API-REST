31708e0dd945526190f725b5c603ba5e164279c4
carlos182 <58117798+carlos182@users.noreply.github.com>
2019-12-20 16:25:08 -0500

    Delete database.config.js

--- config/database.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    /*url: 'mongodb://localhost:27017/apirest'*/
-    url: 'mongodb://apirest:OypSkavueFvpUpIMOQDDCDkYaHiaAhbyYRga3GA2Zlgn52eIOOZ5D7tStPMZFfvUIU5kqHmIqlUOtTZXv2dfpA%3D%3D@apirest.documents.azure.com:10255/?ssl=true'
-}
