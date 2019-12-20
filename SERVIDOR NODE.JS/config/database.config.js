da5a923e55aba84182fdae0bcf8aca35d08c4b3f
carlos182 <58117798+carlos182@users.noreply.github.com>
2019-12-20 16:30:08 -0500

    Add files via upload

---
+++ SERVIDOR NODE.JS/config/database.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    /*url: 'mongodb://localhost:27017/apirest'*/
+    url: 'mongodb://apirest:OypSkavueFvpUpIMOQDDCDkYaHiaAhbyYRga3GA2Zlgn52eIOOZ5D7tStPMZFfvUIU5kqHmIqlUOtTZXv2dfpA%3D%3D@apirest.documents.azure.com:10255/?ssl=true'
+}
